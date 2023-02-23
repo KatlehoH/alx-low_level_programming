f1cd2eefc174137c5acf70d317901f68410452dc
KatlehoH <kat.hlatshwayo@gmail.com>
2023-02-23 07:30:02 -0800

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - adds two values
+ * @n1: first parameter
+ * @n2: second parameter
+ *
+ * Return: sum
+ */
+
+int add(int n1, int n2)
+
+{
+	return (n1 + n2);
+}
